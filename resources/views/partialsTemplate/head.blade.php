b341e5c3be3bdfe434119b47b14aad47af8d2d41
yrvinmolero <yrvinmolero5@gmail.com>
2018-10-28 21:34:09 -0300

    Backup 28/10/2018 21:34

---
+++ resources/views/partialsTemplate/head.blade.php
@@ -0,0 +1,16 @@
+<link rel="stylesheet" type="text/css" href="/css/template/pace-theme-flash.css">
+<script type="text/javascript" src="/js/template/pace.min.js"></script>
+    <!-- Bootstrap CSS-->
+<link rel="stylesheet" type="text/css" href="/css/template/bootstrap.min.css">
+    <!-- Fonts-->
+<link rel="stylesheet" type="text/css" href="/css/template/themify-icons.css">
+    <!-- Malihu Scrollbar-->
+<link rel="stylesheet" type="text/css" href="/css/template/jquery.mCustomScrollbar.min.css">
+    <!-- Animo.js-->
+<link rel="stylesheet" type="text/css" href="/css/template/animate-animo.min.css">
+    <!-- Flag Icons-->
+<link rel="stylesheet" type="text/css" href="/css/template/flag-icon.min.css">
+    <!-- Bootstrap Progressbar-->
+<link rel="stylesheet" type="text/css" href="/css/template/bootstrap-progressbar-3.3.4.min.css">
+    <!-- Primary Style-->
+<link rel="stylesheet" type="text/css" href="/css/template/first-layout.css">
---
+++ resources/views/partialsTemplate/script.blade.php
@@ -0,0 +1,14 @@
+<script type="text/javascript" src="/js/template/jquery.min.js"></script>
+    <!-- Bootstrap JavaScript-->
+ <script type="text/javascript" src="/js/template/bootstrap.min.js"></script>
+    <!-- Malihu Scrollbar-->
+ <script type="text/javascript" src="/js/template/jquery.mCustomScrollbar.concat.min.js"></script>
+    <!-- Animo.js-->
+ <script type="text/javascript" src="/js/template/animo.min.js"></script>
+    <!-- Bootstrap Progressbar-->
+ <script type="text/javascript" src="/js/template/bootstrap-progressbar.min.js"></script>
+    <!-- jQuery Easy Pie Chart-->
+ <script type="text/javascript" src="/js/template/jquery.easypiechart.min.js"></script>
+    <!-- Custom JS-->
+ <script type="text/javascript" src="/js/template/app.js"></script>
+ <script type="text/javascript" src="/js/template/demo.js"></script>
