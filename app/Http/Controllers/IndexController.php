55fa512e8a9e3a6c2e5b586322dc2adc541c7366
malvarez <malvrz08@gmail.com>
2018-11-06 23:48:32 -0300

    Modificacion select de index - se muestran las comunas que tienen publicaciones activas

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -5,13 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Zonas;
 
-
 class IndexController extends Controller
 {
-     public function index()
-     {
-     	$zones = Zonas::where('zonEstado', '=', 'A')->get()->toArray();
-     	return view('index.index', compact('zones'));
-     }
+
+    public function index()
+    {
+        $zones = Zonas::with('pubzonas')->whereHas('pubzonas', function ($zonas) {
+                    $zonas->where('zonEstado', '=', 'A');
+                })->get()->toArray();
+        return view('index.index', compact('zones'));
+    }
 
 }
